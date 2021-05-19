ce74192b2a4c0099cb2c24b90898a2dade3096d8
francobriz <fbzbkr@gmail.com>
2021-05-19 18:18:35 -0300

    cambios en el server

--- server.js
+++ server.js
@@ -13,16 +13,22 @@ app.get("/", function (req, res) {
 });
 
 app.post("/login", function (req, res) {
+  let usuario = req.body.usuario;
+  let contraseña = req.body.contraseña;
 
+  if (usuario === " " || contraseña === " " || !usuario || !contraseña) {
+    console.log("usuario o contraseña vacios");
+    res.sendFile(path.join(__dirname, "cliente/index.html"));
+    return;
+  }
   for (let i = 0; i < users.length; i = i + 2) {
-    
-    if (users[i] === req.body.usuario && users[i + 1] === req.body.contraseña) {
+    if (users[i] === usuario && users[i + 1] === contraseña) {
       res.sendFile(path.join(__dirname, "cliente/bienvenidx.html"));
       console.log(req.body);
-    } else {
-      res.sendFile(path.join(__dirname, "cliente/index.html"));
-      console.log(req.body);
+      return;
     }
+    res.sendFile(path.join(__dirname, "cliente/index.html"));
+    console.log(req.body);
   }
 });
 
@@ -31,21 +37,36 @@ app.get("/registro", function (req, res) {
 });
 
 app.post("/registro", function (req, res) {
-
   let usuario = req.body.nuevoUsuario;
   let contraseña = req.body.nuevaContraseña;
   let repContraseña = req.body.repetirContraseña;
 
-  if (contraseña && repContraseña && usuario && contraseña === repContraseña) {
-    users.push(usuario);
-
-    users.push(contraseña);
-
-    res.sendFile(path.join(__dirname, "cliente/index.html"));
-
-  } else {
+  if (
+    usuario === " " ||
+    contraseña === " " ||
+    repContraseña === " " ||
+    !usuario ||
+    !contraseña ||
+    !repContraseña
+  ) {
     res.sendFile(path.join(__dirname, "cliente/registro.html"));
+    return;
   }
+  for (let i = 0; i < users.length; i = i + 2) {
+    if (users[i] === usuario) {
+      console.log("usuario ya existe");
+      res.sendFile(path.join(__dirname, "cliente/registro.html"));
+      return;
+    }
+  }
+
+  if (contraseña === repContraseña) {
+    users.push(usuario);
+    users.push(contraseña);
+    res.sendFile(path.join(__dirname, "cliente/index.html"));
+    return;
+  }
+  res.sendFile(path.join(__dirname, "cliente/registro.html"));
 });
 
 app.listen(PUERTO, function () {
